071af3ddc5b505d0327b2ef2708fd092f251123d
Tal KRIEF <talkrief@gmail.com>
2023-11-29 15:24:08 +0100

    finished

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,13 @@
 class ReviewsController < ApplicationController
   def new
     # @review.restaurant_id = @restaurant.id
-    @review = Review.find(params[:restaurant_id])
+    # @review = Review.find(params[:restaurant_id])
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
 
   def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
     @review.save
